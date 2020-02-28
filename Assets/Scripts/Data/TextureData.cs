007eaf3f02a3bc45b337d8d2896a225ab58da725
Brian Fairbanks <brian.k.fairbanks@gmail.com>
2020-02-28 12:30:24 -0600

    Texture Data Rewrite
    Updated texture data to be contained in one array instead of 3 seperate fields

--- Assets/Scripts/Data/TextureData.cs
+++ Assets/Scripts/Data/TextureData.cs
@@ -1,16 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 [CreateAssetMenu()]
 public class TextureData : UpdatableData
 {
-    public Color[] baseColors;
-    [Range(0, 1)]
-    public float[] baseStartHeights;
-    
-    [Range(0,1)]
-    public float[] baseBlends;
+
+    public Layer[] layers;
 
     float savedMinHeight;
     float savedMaxHeight;
@@ -19,10 +16,12 @@ public class TextureData : UpdatableData
 
     public void ApplyToMaterial(Material material) {
 
-        material.SetInt("baseColorCount", baseColors.Length);
-        material.SetColorArray("baseColors", baseColors);
-        material.SetFloatArray("baseStartHeights", baseStartHeights);
-        material.SetFloatArray("baseBlends", baseBlends);
+        material.SetInt("layerCount", layers.Length);
+        material.SetColorArray("baseColors", layers.Select(x => x.tint).ToArray());
+        material.SetFloatArray("baseStartHeights", layers.Select(x => x.startHeight).ToArray());
+        material.SetFloatArray("baseBlends", layers.Select(x => x.blendStrength).ToArray());
+        material.SetFloatArray("baseColorStrengths", layers.Select(x => x.tintStrength).ToArray());
+        material.SetFloatArray("baseTextureScales", layers.Select(x => x.textureScale).ToArray());
 
         // adding this, along with the saved details to maintain this information after changes are made to the shader.
         //UpdateMeshHeights(material, savedMinHeight, savedMaxHeight);
@@ -35,5 +34,18 @@ public class TextureData : UpdatableData
 
         material.SetFloat("minHeight", minHeight);
         material.SetFloat("maxHeight", maxHeight);
+    }
+
+    [System.Serializable]
+    public class Layer {
+        public Texture texture;
+        public Color tint;
+        [Range(0,1)]
+        public float tintStrength;
+        [Range(0, 1)]
+        public float startHeight;
+        [Range(0, 1)]
+        public float blendStrength;
+        public float textureScale;
     }
 }
--- Assets/Scripts/Terrain.shader
+++ Assets/Scripts/Terrain.shader
@@ -20,13 +20,16 @@ Shader "Custom/Terrain"
 		float minHeight;
 		float maxHeight;
 
-		const static int maxColorCount = 8;
+		const static int maxLayerCount = 8;
 		const static float epsilon = 1E-4;
 
-		int baseColorCount;
-		float3 baseColors[maxColorCount];
-		float baseStartHeights[maxColorCount];
-		float baseBlends[maxColorCount];
+		int layerCount;
+		float3 baseColors[maxLayerCount];
+		float baseStartHeights[maxLayerCount];
+		float baseBlends[maxLayerCount];
+		float baseColorStrengths[maxLayerCount];
+		float baseTextureScales[maxLayerCount];
+		
 
 		sampler2D testTexture;
 		float testScale;
@@ -49,7 +52,7 @@ Shader "Custom/Terrain"
         void surf (Input IN, inout SurfaceOutputStandard o)
         {
 			float heightPercent = inverseLerp(minHeight, maxHeight, IN.worldPos.y);
-			for (int i = 0; i<baseColorCount; i++){
+			for (int i = 0; i< layerCount; i++){
 				float drawStrength = inverseLerp(-baseBlends[i]/2 - epsilon, baseBlends[i]/2, heightPercent - baseStartHeights[i]);
 				o.Albedo = (o.Albedo *(1-drawStrength)) + (baseColors[i] * drawStrength);
 			}
@@ -65,7 +68,7 @@ Shader "Custom/Terrain"
 			float3 yProjection = tex2D(testTexture, scaledWorldPos.xz) * blendAxes.y;
 			float3 zProjection = tex2D(testTexture, scaledWorldPos.xy) * blendAxes.z;
 			// adding these 3 together can possibly excede color values of 1, making it brighter.  This is corrected by averaging the blendAxes before calculating projections
-			o.Albedo = xProjection + yProjection + zProjection;
+			//o.Albedo = xProjection + yProjection + zProjection;
         }
         ENDCG
     }
